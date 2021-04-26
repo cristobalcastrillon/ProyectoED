6b10c960b6c977ae8688ab88b4b93d911961475b
cristobalcastrillon <44101193+cristobalcastrillon@users.noreply.github.com>
2021-04-26 18:24:48 -0500

    Arreglar HuffmanTree::codificar.

--- HuffmanTree.hpp
+++ HuffmanTree.hpp
@@ -2,6 +2,8 @@
 #include <iostream>
 #include <vector>
 #include <queue>
+#include <unordered_map>
+#include <string>
 
 bool sortbysec(const std::pair<char, int> &a, const std::pair<char, int> &b){
     return (a.second > b.second);
@@ -22,18 +24,14 @@ struct HuffmanTree{
                 HuffmanNode * huffPtr = new HuffmanNode(histoCars.at(i).first, histoCars.at(i).second);
                 histoCopy.push(huffPtr);
             }
- 
+
+            //Creando el árbol (heap)...
             while(histoCopy.size() > 1){
-                std::cout << "Entró al while" << std::endl;
                 HuffmanNode * a = histoCopy.top();
                 histoCopy.pop();
-                //La siguiente línea es de prueba...
-                std::cout << a->caracter << '\t' << a->frecuencia << std::endl;
 
                 HuffmanNode * b = histoCopy.top();
                 histoCopy.pop();
-                //La siguiente línea es de prueba...
-                std::cout << b->caracter << '\t' << b->frecuencia << std::endl;
 
                 HuffmanNode * pater;
                 pater->leftChild = a;
@@ -45,7 +43,29 @@ struct HuffmanTree{
             }
 
             HuffmanNode * root = histoCopy.top();
+            std::unordered_map<char, std::string> huffmanCode;
+            //TODO: Averiguar por qué no llega a las hojas con 'codificar'
+            this->codificar(root, "", huffmanCode);
+
+            for(int i = 0; i < histoCars.size(); i++){
+                std::cout << huffmanCode.at(histoCars.at(i).first) << std::endl;
+            }
         }
 
+        void codificar(HuffmanNode* root, std::string str, std::unordered_map<char, std::string> &huffmanCode){
+            if (!root){
+                std::cout << "Retornó en nullptr" << std::endl;
+                return;
+            }
 
+            if (!root->leftChild && !root->rightChild) {
+                std::cout << "Retornó en hoja" << std::endl;
+                huffmanCode.at(root->caracter) = str;
+            }
+
+            std::cout << "Entró" << std::endl;
+
+            codificar(root->leftChild, str + "0", huffmanCode);
+            codificar(root->rightChild, str + "1", huffmanCode);
+        }
 };
