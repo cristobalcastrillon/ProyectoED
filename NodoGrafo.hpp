8dc943c135aad67705381da6e147b51d8c4945ac
cristobalcastrillon <44101193+cristobalcastrillon@users.noreply.github.com>
2021-05-26 20:46:02 -0500

    Eliminé las líneas que producían el error.

--- NodoGrafo.hpp
+++ NodoGrafo.hpp
@@ -8,15 +8,13 @@ struct NodoGrafo{
     NodoGrafo * izq = NULL;
     NodoGrafo * der = NULL;
 
-    NodoGrafo(char c, int i, int j);
+    NodoGrafo(char c, int i, int j){
+        //Posicion en la matriz:
+        //i: fila
+        //j: columna
+        posicionMatriz[0] = i;
+        posicionMatriz[1] = j;
+        carBase = c;
+        valASCII = int(c);
+    }
 };
-
-NodoGrafo::NodoGrafo(char c, int i, int j){
-    //Posicion en la matriz:
-    //i: fila
-    //j: columna
-    posicionMatriz[0] = i;
-    posicionMatriz[1] = j;
-    carBase = c;
-    valASCII = int(c);
-}
